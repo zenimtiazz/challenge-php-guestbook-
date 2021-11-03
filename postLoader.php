068d6fa2995ca894c2d6e9957b80c763ee88f445
Zain Imtiaz <zenimtiazz@gmail.com>
2021-11-03 14:20:52 +0100

    added the value but still i am getting error

---
+++ postLoader.php
@@ -0,0 +1,46 @@
+<?php
+
+class PostLoader {
+    private $posts = [];
+    //private $contents = 'database.txt';
+    const DB_FILE = 'database.txt';
+
+    public function addPost($title, $content, $author){
+        $newPost = new Post($title, $content, $author);
+        array_push($this->posts, $newPost);
+       
+    }
+
+    public function savePost(){
+        // Encode the posts array to save it to the database.txt file
+        $encodedPosts = serialize($this->posts); // Converts an array or object to a string representation of the object
+        //otherwise we could use json_encode and export
+        
+        //file_put_contents('database.txt', $encodedPosts);
+        //$contents = file_get_contents('database.txt', $encodedPosts);
+        file_put_contents(self::DB_FILE, $encodedPosts);
+    }
+
+    public function getPosts(){
+        return $this->posts; //converts string to array
+    }
+
+    public function __construct() {
+        //$contents = file_get_contents('database.txt');
+        $contents = file_get_contents(self::DB_FILE);
+
+
+        if (!empty($contents)){
+      
+            $this->posts = unserialize($contents); //put contents in posts, converts string to array
+         
+        }
+
+    }
+
+}
+
+
+
+
+
