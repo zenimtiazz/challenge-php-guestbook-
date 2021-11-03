543078edee7d483bee3b70c5e92e5eacd33a14bb
Zain Imtiaz <zenimtiazz@gmail.com>
2021-11-03 14:22:03 +0100

    linked all the files

---
+++ index.php
@@ -0,0 +1,23 @@
+  
+<?php
+
+//we are going to use session variables so we need to enable sessions
+// session_start();
+
+// function whatIsHappening()
+// {
+//     echo '<h2>$_GET</h2>';
+//     var_dump($_GET);
+//     echo '<h2>$_POST</h2>';
+//     var_dump($_POST);
+//     echo '<h2>$_COOKIE</h2>';
+//     var_dump($_COOKIE);
+//     echo '<h2>$_SESSION</h2>';
+//     var_dump($_SESSION);
+// }
+
+// whatIsHappening(); // call function
+
+require 'post.php';
+require 'postLoader.php';
+require 'homepage.php';
