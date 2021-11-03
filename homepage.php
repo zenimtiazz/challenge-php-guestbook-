db16b28ae2b796d6d8ed9ad7e279819a28a4464e
Zain Imtiaz <zenimtiazz@gmail.com>
2021-11-03 12:49:51 +0100

    changes in homepage

--- homepage.php
+++ homepage.php
@@ -1,42 +1,82 @@
+<?php
+
+//instead of isset:
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    //if above is true then way we have access to form input
+
+    //Make sure the script can handle site defacement attacks: use htmlspecialchars()
+    $title = trim(htmlspecialchars($_POST['title']));
+    $content = trim(htmlspecialchars($_POST['content']));
+    $author = trim(htmlspecialchars($_POST['author']));
+    //trim removes spaces left and right
+
+    $loader = new PostLoader();
+    //instantiated PostLoader and have access to properties of the class PostLoader
+    $loader->addPost($title, $content, $author);
+    $loader->savePost();
+
+    $posts = $loader->getPosts();
+
+    //The messages are sorted from new (top) to old (bottom).
+    $posts = array_reverse($posts); //store reversed array in variable
+
+    //Only show the latest 20 posts.
+    $posts = array_slice($posts, 0, 20);
+    //makes new array
+
+    //echo 'length of the array: ' .count($posts);
+
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" type="text/css"
-          rel="stylesheet"/>
-    <title> Guestbook</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class = "container">
-        <h1>Guestbook</h1>
-        <form method = "post" action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-        <div class="form-row">
-            <div class="form-group col-md-4">
-                <label for="title">Title:</label>
-                <input type="text" id="title" name="title" class="form-control" placeholder="" value="" autofocus>
-            </div>
-            <div class="form-group col-md-4">
-                <label for="date">Date:</label>
-                <input type="text" id="date" name="date" class="form-control" placeholder="" value="">
-            </div>
-            <div class="form-group col-md-4">
-                <label for="author">Author:</label>
-                <input type="text" id="author" name="author" class="form-control" placeholder="" value="">
-            </div>
-        </div>
-        <div class="form-row">
-            <div class="form-group col-md-12">
-                <label for="content">Content:</label>
-                <textarea id="content" name="content" rows="2" class="form-control" placeholder="">
-                </textarea>
-            </div>
-        </div>
 
-        <button type="submit" class="btn btn-primary">Add Post</button>
+
+<form method="POST">
+    <label for="author">Author:
+        <input type="text" name="author" id="author">
+    </label>
+    <label for="author">Title:
+        <input type="text" name="title" id="title">
+    </label>
+    <label for="author">Message:
+        <input type="text" name="content" id="content">
+    </label>
+    <button type="submit">Save</button>
 </form>
+
+<div id="posts-wrapper">
+
+    <?php
+
+    if (!empty($posts)) {
+
+        foreach ($posts as $post) {
+            ?>
+            <div class="single-post">
+
+                <p><?php echo $post->getTitle()?></p>;
+                <p><?php echo $post->getAuthor()?></p>;
+              
+                <p><?php echo $post->getContent()?></p>;
+
+            </div>
+            <?php
+        }
+    }
+
+    ?>
+
 </div>
+
 </body>
 </html>
